d748c747c42aa1e881071051d94c1bc4cfce59b9
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-07-05 15:09:14 -0300

    Added test files
    radio test and how to send then to the server

---
+++ radio-test/test.php
@@ -0,0 +1,15 @@
+<?php $container_size = $_POST["radio-size"]; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test part 2</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<main>
+			<p>
+				<?php echo $container_size; ?>
+			</p>
+		</main>
+	</body>
+
